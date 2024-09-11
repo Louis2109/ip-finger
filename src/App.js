e42fdec8b0e326b24fcf167dc12a48fd035f37df
Louis2109 <nkenfacklandoloic@gmail.com>
2024-09-11 13:10:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 50mil ce mois
         </a>
       </header>
     </div>
